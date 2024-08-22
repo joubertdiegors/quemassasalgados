74283ed9a004e9f07e5dbaf87f70a77f7dae20f1
Joubert Diego <jdrsjdrsoares@gmail.com>
2024-08-23 01:46:16 +0200

    ajuste urls products

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.ProductListView.as_view(), name='product_list'),
-    path('create/', views.ProductCreateView.as_view(), name='product_create'),
-    path('edit/<int:pk>/', views.ProductUpdateView.as_view(), name='product_edit'),
+    path('product/list/', views.ProductListView.as_view(), name='product_list'),
+    path('product/create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('product/edit/<int:pk>/', views.ProductUpdateView.as_view(), name='product_edit'),
 ]
