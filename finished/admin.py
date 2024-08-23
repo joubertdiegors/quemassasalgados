18e90458fa1c4818b2a5a058a2ae012dc5a283c4
Joubert Diego <jdrsjdrsoares@gmail.com>
2024-08-23 23:35:49 +0200

    ajuste admin finished

--- finished/admin.py
+++ finished/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import FinishedOrder, FinishedProduct
 
-# Register your models here.
+admin.site.register(FinishedOrder)
+admin.site.register(FinishedProduct)
